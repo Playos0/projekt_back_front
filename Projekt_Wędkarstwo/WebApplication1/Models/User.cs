26b6b84dfecc481f919a7ad3171d0a2df5b9246c
Playos0 <krzysiek.1.k@wp.pl>
2026-03-24 01:30:58 +0100

    Add two new Models
    -First look on User and Product Models

---
+++ Projekt_Wędkarstwo/WebApplication1/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Email { get; set; }
+
+        public string? Address { get; set; }
+
+      
+
+    }
+}
