f35c99ba877bd33d1298e09c5a161f90f652c18a
Playos0 <krzysiek.1.k@wp.pl>
2026-03-31 17:47:43 +0200

    Add LoginRequest, RegisterRequest
    Two DTO interfaces have been added to the repo to handle data transfers between Back and Front.

---
+++ Projekt_Wędkarstwo/WebApplication1/Models/DTOs/LoginRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models.DTOs
+{
+    public class LoginRequest
+    {
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public required string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public required string Password { get; set; }
+
+
+    }
+}
---
+++ Projekt_Wędkarstwo/WebApplication1/Models/DTOs/RegisterRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models.DTOs
+{
+    public class RegisterRequest
+    {
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public required string Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(100)]
+        public required string Password { get; set; }
+
+    }
+}
