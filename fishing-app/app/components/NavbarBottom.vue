36d3e0a82bacb828b50e910cad0e68cf782cfa3b
SteveRacks <danielholowczyc04@gmail.com>
2026-05-03 20:06:47 +0200

    navbar

---
+++ fishing-app/app/components/NavbarBottom.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+</script>
+
+<template>
+  <nav class="navbarBottom">
+    <div class="menu">
+      <Icon icon="mdi:storefront-outline" />
+      Sklep <Icon icon="mdi:chevron-down" />
+      <div class="dropdown">
+        <NuxtLink class="navItem" to="/rods">Wędki</NuxtLink>
+        <NuxtLink class="navItem" to="/reels">Kołowrotki</NuxtLink>
+        <NuxtLink class="navItem" to="/lures">Przynęty</NuxtLink>
+        <NuxtLink class="navItem" to="/accessories">Akcesoria</NuxtLink>
+      </div>
+    </div>
+    <NuxtLink class="navItem" to="/fishing-grounds">
+      <Icon icon="lucide-lab:pond" />
+      Łowiska</NuxtLink
+    >
+    <NuxtLink class="navItem" to="/boats">
+      <Icon icon="mdi:sail-boat" />
+      Łódki</NuxtLink
+    >
+    <NuxtLink class="navItem" to="/subscriptions">
+      <Icon icon="mdi:subscriptions" />
+      Subskrypcje</NuxtLink
+    >
+  </nav>
+</template>
+
+<style scoped>
+.navbarBottom {
+  background-color: #112a2d;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 20px;
+  padding: 10px 20px;
+  z-index: 1000;
+  position: sticky;
+}
+a {
+  color: #f1f1f1;
+  text-decoration: none;
+}
+a:hover {
+  color: #78c0a8;
+}
+.menu {
+  position: relative;
+  cursor: pointer;
+  padding: 10px 0;
+}
+.dropdown {
+  position: absolute;
+  background-color: #112a2d;
+  top: 90%;
+  left: 0;
+  padding: 10px;
+  border-radius: 5px;
+  min-width: 150px;
+  flex-direction: column;
+  opacity: 0;
+  transform: translateY(-10px);
+  transition: all 0.2s ease;
+}
+.dropdown a {
+  display: block;
+  padding: 5px 0;
+}
+
+.menu:hover .dropdown {
+  opacity: 1;
+  transform: translateY(0);
+}
+.navItem svg {
+  margin-right: 5px;
+}
+</style>
---
+++ fishing-app/app/components/NavbarTop.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { useCartStore } from "@/stores/cart";
+import { Icon } from "@iconify/vue";
+const cart = useCartStore();
+</script>
+
+<template>
+  <nav class="navbarTop">
+    <NuxtLink to="/">
+      <Icon icon="mdi:fishbowl-outline" />
+      <span class="brand">Wędkarski Świat</span>
+    </NuxtLink>
+    <div class="search">
+      <input type="text" placeholder="Search..." />
+      <button>
+        <Icon icon="mdi:magnify" />
+      </button>
+    </div>
+    <NuxtLink class="navItem" to="/login">
+      <Icon icon="mdi:user-circle" />
+      Login</NuxtLink
+    >
+    <NuxtLink class="navItem" to="/contact">
+      <Icon icon="mdi:phone-in-talk" />
+      Kontakt</NuxtLink
+    >
+    <NuxtLink class="navItem" to="/cart">
+      <Icon icon="mdi:shopping-cart" />
+      Koszyk ({{ cart?.items?.length || 0 }})</NuxtLink
+    >
+  </nav>
+</template>
+
+<style scoped>
+.navbarTop {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #1a3c40;
+  padding: 15px;
+  font-size: 20px;
+  font-weight: 750;
+  /* background-image: url('rybak.jpg'); */
+  background-size: cover;
+  background-position: center;
+}
+.brand {
+  letter-spacing: 3px;
+}
+.navItem:hover {
+  color: #78c0a8;
+}
+a {
+  color: white;
+  text-decoration: none;
+}
+.search {
+  background: rgba(255, 255, 255, 0.1);
+  border: 1px solid #78c0a8;
+  color: white;
+  placeholder {
+    color: #ccc;
+  }
+}
+.icon {
+  font-weight: 1000;
+}
+</style>
