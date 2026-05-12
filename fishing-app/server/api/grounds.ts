bc1a4510f48cc566167942a3379d44c77d12789d
Playos0 <83062829+Playos0@users.noreply.github.com>
2026-05-12 14:38:33 +0200

    Delete fishing-app directory

--- fishing-app/server/api/grounds.ts
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler(() => {
-  return [
-    { id: 1, name: "Staw", price: 50 },
-    { id: 2, name: "Jezioro", price: 30 },
-    { id: 3, name: "Ściek", price: 20 },
-  ];
-});
