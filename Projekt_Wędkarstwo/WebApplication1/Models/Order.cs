b1e0cb30d9218a0c116327f78aef53e3aca98c87
Playos0 <krzysiek.1.k@wp.pl>
2026-05-18 19:54:09 +0200

    Add validation and navigation to Order models
    Introduce data annotations and required members to Order and OrderItem models

--- Projekt_Wędkarstwo/WebApplication1/Models/Order.cs
+++ Projekt_Wędkarstwo/WebApplication1/Models/Order.cs
@@ -1,8 +1,22 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class Order
     {
+        int Id { get; set; }
 
-        public List<OrderItem> OrderItems { get; set; }
+        [Required]
+        public required int UserId { get; set; }
+
+        public User User { get; set; } = null!;
+
+        [Required]
+        public required Decimal TotalPrice { get; set; }
+
+        public string Status { get; set; } = "Pending";
+
+        [Required]
+        public required List<OrderItem> OrderItems { get; set; } = new();
     }
 }
--- Projekt_Wędkarstwo/WebApplication1/Models/OrderItem.cs
+++ Projekt_Wędkarstwo/WebApplication1/Models/OrderItem.cs
@@ -1,6 +1,25 @@
-﻿namespace WebApplication1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
 {
     public class OrderItem
     {
+        public int Id { get; set; }
+
+        [Required]
+        public required int OrderId { get; set; }
+
+        public Order Order { get; set; } = null!;
+
+        [Required]
+        public required int ProductId { get; set; }
+
+        public Product Product { get; set; } = null!;
+
+        [Required]
+        public required int Quantity { get; set; }
+
+        [Required]
+        public required decimal UnitPrice { get; set; }
     }
 }
