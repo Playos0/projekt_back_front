193866f3a4b9c4865b573ac3c325f4757708ffa1
Playos0 <krzysiek.1.k@wp.pl>
2026-06-02 01:11:21 +0200

    Add SubscriptionPlan model
    Introduce SubscriptionPlan entity to represent subscription plans for fishing spots. Includes Id, required FishingSpotId (FK), FishingSpot navigation property, required Name, DurationInDays and Price with DataAnnotations and C# 'required' modifiers to enforce non-nullable inputs. Adds System.ComponentModel.DataAnnotations import and places the model in the WebApplication1.Models namespace.

---
+++ Projekt_Wędkarstwo/WebApplication1/Models/SubscriptionPlan.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class SubscriptionPlan
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required int FishingSpotId { get; set; }
+
+        public FishingSpot FishingSpot { get; set; } = null!;
+
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public int DurationInDays { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
