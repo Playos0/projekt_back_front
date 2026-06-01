466ea0ea4bbd79bd5bd20053ecc43f8409447e16
Playos0 <krzysiek.1.k@wp.pl>
2026-06-02 01:04:32 +0200

    Add FishingSpot model
    Introduce a new FishingSpot model in Models/FishingSpot.cs. Defines properties for Id, Name, Description, Location, SubscriptionAvailable and DayPassPrice; applies DataAnnotations [Required] and uses C# 'required' modifier for mandatory properties, with Description nullable.

---
+++ Projekt_Wędkarstwo/WebApplication1/Models/FishingSpot.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class FishingSpot
+    {
+        int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public required string Location { get; set; }
+
+        [Required]
+        public required bool SubscriptionAvailable { get; set; }
+
+        [Required]
+        public required decimal DayPassPrice { get; set; }
+    }
+}
