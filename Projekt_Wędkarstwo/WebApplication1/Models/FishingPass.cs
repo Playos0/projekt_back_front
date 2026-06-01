a092e6a375a4db312e84593bb960796ef1460672
Playos0 <krzysiek.1.k@wp.pl>
2026-06-02 01:08:47 +0200

    Add FishingPass model
    Add new FishingPass entity to WebApplication1.Models. Defines Id, required UserId, User navigation property, ValidDate, Price, and PurchaseDate with [Required] attributes and C# 'required' modifiers to enforce non-nullable values.

---
+++ Projekt_Wędkarstwo/WebApplication1/Models/FishingPass.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class FishingPass
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required int UserId { get; set; }
+
+        public User User { get; set; } = null!;
+
+        [Required]
+        public required DateTime ValidDate { get; set; }
+
+        [Required]
+        public required decimal Price { get; set; }
+
+        [Required]
+        public required DateTime PurchaseDate { get; set; }
+    }
+}
